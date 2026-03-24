e65195ef21db4c048db14b66baeba9e84b1696b4
Artyom-vv <76263393+Artyom-vv@users.noreply.github.com>
2026-03-24 12:30:12 +0300

    .

--- assets/icons/index.ts
+++ assets/icons/index.ts
@@ -1,7 +1,8 @@
 export { default as Logo } from "./logo.svg";
 export { default as LogoWithText } from "./logoWithText.svg";
-export { default as Pattern } from "./Pattern.svg";
+export { default as Pattern } from "./pattern.svg";
 export { default as AddLine } from "./add-line.svg";
 export { default as SubtractLine } from "./subtract-line.svg";
 export { default as MenuLine } from "./menu-line.svg";
 export { default as PhoneFill } from "./phone-fill.svg";
+export { default as Quote } from "./quote.svg";
